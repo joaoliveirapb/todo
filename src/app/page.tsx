400d8e998fce311028d68c2a70e99d07614cedb4
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-03 21:19:44 -0300

    feat: created footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full max-w-[608px] pt-8">
+    <main className="flex min-h-screen w-full max-w-[608px] flex-col py-8">
       <Header />
+      <div className="flex-1"></div>
+      <Footer />
     </main>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import { Heart } from 'lucide-react'
+import Link from 'next/link'
+
+export function Footer() {
+  return (
+    <footer className="flex items-center justify-center text-sm text-zinc-500 dark:text-zinc-400">
+      created with <Heart size={18} className="mx-0.5 text-violet-500" /> by
+      <Link
+        href="https://github.com/joaoliveirapb"
+        className="ml-0.5 font-bold underline"
+      >
+        @joaoliveirapb
+      </Link>
+    </footer>
+  )
+}
