627a52d08d68553773c5640217445272860e1cb5
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-06 17:09:34 -0300

    feat: delete all button in completed section

--- src/components/CompletedTodoList.tsx
+++ src/components/CompletedTodoList.tsx
@@ -1,13 +1,30 @@
 import { useTasksContext } from '@/context/TasksContext'
+import { Trash2 } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import { TodoItem } from './TodoItem'
 
 export function CompletedTodoList() {
+  const [isDeleteAllButtonVisible, setIsDeleteAllButtonVisible] =
+    useState<boolean>(false)
   const { allTodos } = useTasksContext()
+
+  useEffect(() => {
+    const hasCompletedTodos = allTodos.some((todo) => todo.checked)
+    setIsDeleteAllButtonVisible(hasCompletedTodos)
+  }, [allTodos])
 
   return (
     <>
       {allTodos.map(
         (todo) => todo.checked && <TodoItem key={todo.id} todo={todo} />,
+      )}
+      {isDeleteAllButtonVisible && (
+        <div className="mt-6 flex justify-end">
+          <button className="flex items-center gap-1 rounded-xl bg-red-500 px-8 py-4 text-white transition-colors hover:bg-red-600">
+            <Trash2 size={14} />
+            <span className="text-xs font-semibold">delete all</span>
+          </button>
+        </div>
       )}
     </>
   )
