9d9b4381b7c7cab460f10ec767a9930b410bc0b5
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-07 22:55:41 -0300

    refactor: heart icon color changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export function Footer() {
   return (
     <footer className="flex items-center justify-center text-sm text-zinc-500 dark:text-zinc-400">
-      created with <Heart size={18} className="mx-0.5 text-violet-500" /> by
+      created with <Heart size={18} className="mx-0.5 text-red-500" /> by
       <Link
         href="https://github.com/joaoliveirapb"
         className="ml-0.5 font-bold underline"
