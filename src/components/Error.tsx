497f050dee267cdfb22d1b59f5c235a0ff29ae39
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-10 18:11:43 -0300

    refactor: updated error message

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,7 +1,7 @@
 export function Error() {
   return (
     <p className="text-sm font-medium text-red-500">
-      Digite a tarefa que deseja adicionar.
+      Write the task you want to add.
     </p>
   )
 }
